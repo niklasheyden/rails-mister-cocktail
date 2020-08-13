092010f33af91df4cc67da084497bd720bfba250
Niklas von Heyden <niklas.heyden@me.com>
2020-08-13 15:37:22 +0200

    mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts "Cleaning database..."
+Cocktail.destroy_all
+puts "Creating some cocktails..."
+10.times do
+  cocktail = Cocktail.create(
+    name: Faker::FunnyName.two_word_name
+    )
+  puts "Cocktail with id #{cocktail.id} was created"
+end
+puts "Created #{Cocktail.count} cocktails"
