12d4808582882bbcbc95ed7730be7849a02b96c6
Niklas von Heyden <niklas.heyden@me.com>
2020-08-14 15:45:41 +0200

    add cocktail images

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,6 +9,7 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
